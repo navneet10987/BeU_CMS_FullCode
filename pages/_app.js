5a8f163f1bd07ced908faeefcdca0884b0dc9ee5
navneet10987 <navneet10987@gmail.com>
2025-07-01 16:46:55 +0530

    Fix:Correct _app.js syntax for build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-// pages/_app.js
-import '../styles/globals.module.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
-echo "// trigger rebuild" >> pages/_apps.js
